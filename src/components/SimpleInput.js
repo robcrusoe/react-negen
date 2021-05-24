4e8da651abc8ad1ba49e5605e53567018b5ed4dd
Arka Sain <arka.sain@aol.com>
2021-05-24 09:58:04 +0200

    Working with `useRef()` - not recommended *

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,20 @@
+import { useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(nameInputRef.current.value);
+    nameInputRef.current.value = '';
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={nameInputRef} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
